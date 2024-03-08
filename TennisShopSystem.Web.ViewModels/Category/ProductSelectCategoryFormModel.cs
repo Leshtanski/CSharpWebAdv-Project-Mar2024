8f8bf3eb9298543301506f4d661d241d28b8b06a
LubomirLeshtanski <lubomir.leshta@gmail.com>
2024-03-08 16:42:31 +0200

    Created add product form model and validations for adding new Product

---
+++ TennisShopSystem.Web.ViewModels/Category/ProductSelectCategoryFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TennisShopSystem.Web.ViewModels.Category
+{
+    public class ProductSelectCategoryFormModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ TennisShopSystem.Web.ViewModels/Product/ProductFormModel.cs
@@ -0,0 +1,37 @@
+﻿namespace TennisShopSystem.Web.ViewModels.Product
+{
+    using System.ComponentModel.DataAnnotations;
+    using TennisShopSystem.Web.ViewModels.Category;
+
+    using static TennisShopSystem.Common.EntityValidationConstants.Product;
+
+    public class ProductFormModel
+    {
+        public ProductFormModel()
+        {
+            this.Categories = new HashSet<ProductSelectCategoryFormModel>();
+        }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [StringLength(ImageUrlMaxLength)]
+        [Display(Name = "Image Link")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Range(typeof(decimal), PriceMinValue, PriceMaxValue)]
+        [Display(Name = "Price Per Item")]
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<ProductSelectCategoryFormModel> Categories { get; set; }
+    }
+}
