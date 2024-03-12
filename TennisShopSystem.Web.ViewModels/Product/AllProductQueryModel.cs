06c4e6bb29cdbe12590467adeb239e5ed1cd1aef
LubomirLeshtanski <lubomir.leshta@gmail.com>
2024-03-12 17:19:06 +0200

    Added Product Sorting enumeration. Added All Product Query Model and Product All View Model. Added two new constants in General Application Constants.

---
+++ TennisShopSystem.Web.ViewModels/Product/AllProductQueryModel.cs
@@ -0,0 +1,42 @@
+﻿namespace TennisShopSystem.Web.ViewModels.Product
+{
+    using System.ComponentModel.DataAnnotations;
+    using TennisShopSystem.Web.ViewModels.Product.Enums;
+
+    using static TennisShopSystem.Common.GeneralAppicationConstants;
+
+    public class AllProductQueryModel
+    {
+        public AllProductQueryModel()
+        {
+            this.CurrentPage = DefaultPage;
+            this.ProductsPerPage = EntitiesPerPage;
+
+            this.Categories = new HashSet<string>();
+            this.Brands = new HashSet<string>();
+            this.Products = new HashSet<ProductAllViewModel>();
+        }
+
+        public string? Category { get; set; }
+
+        public string? Brand { get; set; }
+
+        [Display(Name = "Search by keyword")]
+        public string? SearchString { get; set; }
+
+        [Display(Name = "Sort By")]
+        public ProductSorting ProductSorting { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int ProductsPerPage { get; set; }
+
+        public int TotalProducts { get; set; }
+
+        public IEnumerable<string> Categories { get; set; }
+
+        public IEnumerable<string> Brands { get; set; }
+
+        public IEnumerable<ProductAllViewModel> Products { get; set; }
+    }
+}
---
+++ TennisShopSystem.Web.ViewModels/Product/ProductAllViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TennisShopSystem.Web.ViewModels.Product
+{
+    public class ProductAllViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        [Display(Name = "Image Link")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Display(Name = "Price per item")]
+        public decimal Price { get; set; }
+    }
+}
